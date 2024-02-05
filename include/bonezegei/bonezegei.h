bdc80b4e11e528945b01c3e13795dd08fe524565
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-02-06 00:26:10 +0800

    Update bonezegei.h

--- include/bonezegei/bonezegei.h
+++ include/bonezegei/bonezegei.h
@@ -1,1 +1,32 @@
+/*
+    Author: (Bonezegei) Jofel Batutay
+    Date: February 4, 2024
+    Purpose: Bonezegei Engine
+*/
+
+#include "driver/driver.h"
+#include "driver/driverOpenGL.h"
+#include "device/device.h"
+#include "device/deviceWindows.h"
+#include "font/font.h"
+#include "font/fontArray.h"
+#include "image/image.h"
+#include "image/imageBMP.h"
+#include "image/texture.h"
+#include "gui/gui.h"
+#include "gui/sgui.h"
+#include "util/event.h"
+#include "driver/draw.h"
+#include "2d/2d.h"
+
+class Bonezegei{
+    public:
+        Bonezegei();
+        void create(Dimension2di res, char *name);
+        void run();
+
+        Device *device;
+        Driver *driver;
+        GUI *ui;
+};
 
