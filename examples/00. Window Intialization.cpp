8039588eb16d4f4278f696fd6b305e0b58c91c04
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-02-06 00:30:40 +0800

    Update 00. Window Intialization.cpp

--- examples/00. Window Intialization.cpp
+++ examples/00. Window Intialization.cpp
@@ -8,7 +8,11 @@
 Bonezegei bzn;
 
 int main(){
+    //! create ( Param1, Param2)
+    //!     Param1 Dimension( Width , Height) 
+    //!     Param2 Window Name
     bzn.create(Dimension2di(1366,768),"Bonezegei Engine");
+    //! run() draw all scenes
     bzn.run();
     return 0;
 }
