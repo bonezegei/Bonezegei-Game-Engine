9076dec415721f35027869304d93069da37777d1
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-02-06 00:31:12 +0800

    Update 01 Window with Loop.cpp

--- examples/01 Window with Loop.cpp
+++ examples/01 Window with Loop.cpp
@@ -8,9 +8,13 @@
 Bonezegei bzn;
 
 int main(){
+    
+    //! create ( Param1, Param2)
+    //!     Param1 Dimension( Width , Height) 
+    //!     Param2 Window Name
     bzn.create(Dimension2di(1366,768),"Bonezegei Engine");
   
-    while(bzn.device->run()){  // Instead of bzn.run(); 
+    while(bzn.device->run()){    // Instead of bzn.run(); 
         bzn.driver->begin();
             //loop code here
         bzn.driver->end();
