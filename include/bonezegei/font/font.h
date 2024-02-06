f7a403f355e67618e12114b5b0d3a61150ec91ac
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-02-07 02:34:06 +0800

    Create font.h

---
+++ include/bonezegei/font/font.h
@@ -0,0 +1,25 @@
+#ifndef __FONT_H_INCLUDED__
+#define __FONT_H_INCLUDED__
+
+#include "../util/color.h"
+#include "../util/vector2d.h"
+#include "../util/dimension2d.h"
+#include "../util/rect.h"
+#include "../image/texture.h"
+//#include "../driver/driver.h"
+
+class Font{
+    public:
+        Font(){}
+        virtual void   create(char fd[], int desc[95][3]){}
+        virtual void   drawText2D(Vector2di pos, char *text, Color color){}
+
+        int H;
+        Texture ftext;
+        int tID;
+        Recti rect[95];
+};
+
+
+#endif
+
